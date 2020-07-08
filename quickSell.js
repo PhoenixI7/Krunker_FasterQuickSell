fd7f2b25c52f11906620a9804dc717831d1a1b63
PhoenixI7 <37677140+PhoenixI7@users.noreply.github.com>
2020-07-07 21:34:00 -0400

    Update quickSell.js

--- quickSell.js
+++ quickSell.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Krunker Market Quick Sell
 // @namespace    http://tampermonkey.net/
-// @version      v1.0
+// @version      v1.1
 // @description  Script for faster quick selling!
 // @author       Phoenixi7
 // @match        https://krunker.io/social.html?p=market
@@ -16,22 +16,20 @@
     let excludedNames = ['Fabrica', 'Lumus', 'Sugarbear', 'Suede Blade', 'Phase Blases', 'Urban Soldier']; /* Capitalize First Letter, Ex. ['Sugarbear', 'Suede Blade'] */
 
     document.addEventListener('keydown', (event) => {
-        if (event.key == "@") {
-            var itemsString = document.getElementById('invTrack').innerHTML.split('<')[0];
-            var itemsInt = parseInt(itemsString);
+        if (event.key == "1") {
+            let itemsString = document.getElementById('invTrack').innerHTML.split('<')[0];
+            let itemsInt = parseInt(itemsString);
             promptOptions(itemsInt + 10); //add 10 bc sometimes krunker item numbers are more than invTrack
         }
     });
 
     function promptOptions(items) {
-        var input = prompt('To sell all type "ALL"...\nTo go one at a time type "ONE"...\nTo exit type "EXIT"...');
+        let input = prompt('To sell all type "ALL"...\nTo go one at a time type "ONE"...\nTo exit type "EXIT"...');
         if (input == "ALL") {
-            var check = prompt('Are you sure you want to quick sell everything? \nType "YES" to continue');
+            const check = confirm('Are you sure you want to quick sell everything?');
             if (check == "YES") {
                 quickSellAll(items);
-            } else {
-                promptOptions(items);
-            }
+            } else { promptOptions(items); }
         } else if (input == "ONE") {
             quickSell(items);
         } else if (input == "EXIT") {
@@ -53,52 +51,47 @@
         }
     }
 
+    async function sellItem(itemNumber) {
+        document.getElementById('itemCardinventory_' + itemNumber).querySelector('.cardActions').getElementsByClassName('cardAction')[1].click();
+        document.getElementById('confirmBtn').click();
+        await timer(200);
+    }
+
     async function quickSellAll(items) {
-        for (var i = items; i > 0; i--) {
+        for (let i = items; i > 0; i--) {
             if (document.getElementById('itemCardinventory_' + i)) {
-                var itemInfo = getItemInfo(i);
-                if (!excluded(itemInfo)) {
-                    document.getElementById('itemCardinventory_' + i).querySelector('.cardActions').getElementsByClassName('cardAction')[1].click();
-                    document.getElementById('confirmBtn').click();
-                }
+                const itemInfo = getItemInfo(i);
+                if (!excluded(itemInfo)) { await sellItem(i); }
             }
-            await timer(200);
-        }
+        } alert("DONE!");
     }
 
     async function quickSell(items) {
-        for (var i = items; i > 0; i--) {
+        for (let i = items; i > 0; i--) {
             if (document.getElementById('itemCardinventory_' + i)) {
-                var itemInfo = getItemInfo(i);
+                const itemInfo = getItemInfo(i);
                 if (!excluded(itemInfo)) {
-                    var confirmSell = confirm("Sell " + itemInfo[0] + ", it is " + itemInfo[1]);
-                    if (confirmSell) {
-                        document.getElementById('itemCardinventory_' + i).querySelector('.cardActions').getElementsByClassName('cardAction')[1].click();
-                        document.getElementById('confirmBtn').click();
-                    } else {
-                        console.log("Chose not to sell");
-                    }
+                    let confirmSell = confirm("Sell " + itemInfo[0] + ", it is " + itemInfo[1]);
+                    if (confirmSell) { await sellItem(i); }
                 }
             }
-            await timer(200);
-        }
+        } alert("DONE!");
     }
 
     function getItemInfo(itemNumber) {
-        console.log('itemCardinventory_' + itemNumber);
-        var itemName = document.getElementById('itemCardinventory_' + itemNumber).innerHTML.split('<')[0]
-        var rarity = howRare(itemNumber);
-        var info = [itemName, rarity[0], rarity[1], rarity[2]];
+        let itemName = document.getElementById('itemCardinventory_' + itemNumber).innerHTML.split('<')[0]
+        const rarity = howRare(itemNumber)
+        const info = [itemName, rarity[0], rarity[1], rarity[2]]; // Ex. ["Sugarbear", "relic", "rgb(237, 66, 66)", 6]
         return info;
     }
 
     function howRare(itemNumber) {
-        var colors = ["rgb(41, 41, 41)", "rgb(237, 66, 66)", "rgb(251, 192, 45)", "rgb(224, 64, 251)", "rgb(33, 150, 243)", "rgb(178, 242, 82)"];
-        var rarities = ['contraband', 'relic', 'legendary', 'epic', 'rare', "uncommon"];
-        for (var i = 0; i < rarities.length; i++) {
-            var itemColor = document.getElementById('itemCardinventory_' + itemNumber).style.color;
+        let colors = ["rgb(41, 41, 41)", "rgb(237, 66, 66)", "rgb(251, 192, 45)", "rgb(224, 64, 251)", "rgb(33, 150, 243)", "rgb(178, 242, 82)"];
+        let rarities = ['contraband', 'relic', 'legendary', 'epic', 'rare', "uncommon"];
+        for (let i = 0; i < rarities.length; i++) {
+            let itemColor = document.getElementById('itemCardinventory_' + itemNumber).style.color;
+            const rarityInfo = [rarities[i], itemColor, itemNumber]; // Ex. ["relic", "rgb(237, 66, 66)", 6]
             if (itemColor == colors[i]) {
-                var rarityInfo = [rarities[i], itemColor, itemNumber];
                 return rarityInfo;
             }
         }
